ccac4e69f1e2e17e2b7b3d5e2eaf869e3442962e
Praveen Kumar Bonakurthi <prbona@microsoft.com>
2024-01-19 15:20:59 -0800

    Framework test

---
+++ MultiFrameworkLibrary/Framework.cs
@@ -0,0 +1,21 @@
+﻿
+namespace MultiFrameworkLibrary
+{
+    public class Framework
+    {
+        public void PrintFramework() {
+            Console.WriteLine(GetFramework());
+        }
+
+        private string GetFramework()
+        {
+#if NET8_0
+            return "NET8";
+#elif NET6_0_OR_GREATER
+            return "NET6_OR_Greater";
+#else
+            return "Unknown";
+#endif
+        }
+    }
+}
